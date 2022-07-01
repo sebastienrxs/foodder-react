9f041d0a98024a406d0bd64c3c68f522fa7b5477
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-07-01 17:35:10 +0200

    favorites context kinda working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react"
 import { Link, Outlet } from "react-router-dom"
-import { AuthContext } from "../context/auth.context"
 
 function Navbar() {
-  const { isLoggedIn, user, removeToken } = useContext(AuthContext)
-
   return (
     <>
       <nav>
--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -1,5 +1,6 @@
 import { useCallback, useContext, useEffect, useState } from "react"
 import { AuthContext } from "../context/auth.context"
+import { FavContext } from "../context/fav.context"
 import { Navigate } from "react-router-dom"
 import axios from "axios"
 import { API_URL } from "../utils/constants"
@@ -8,14 +9,15 @@ console.log("API_URL:", API_URL)
 
 function FeedPage() {
   const { user, isLoggedIn, isLoading } = useContext(AuthContext)
+  const { userFavorites } = useContext(FavContext)
 
   const [articles, setArticles] = useState([])
-  console.log("articles:", articles)
+  // console.log("articles:", articles)
   const { getToken } = useContext(AuthContext)
 
   const getAllArticles = () => {
     const storedToken = getToken()
-    console.log("storedToken:", storedToken)
+    // console.log("storedToken:", storedToken)
 
     axios
       .get(`${API_URL}/articles`, {
@@ -35,7 +37,7 @@ function FeedPage() {
       {/* <p>You are logged in as {user.payload.username} </p> */}
 
       {articles.map((article) => {
-        console.log("article:", article)
+        // console.log("article:", article)
         return <ArticleCard key={article._id} {...article} />
       })}
     </div>
