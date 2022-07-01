beaaf2e9b6daf49fa6782eca884b26b2d0205aec
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-07-01 11:00:00 +0200

    test 1

--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -4,16 +4,18 @@ import { Navigate } from "react-router-dom"
 import axios from "axios"
 import { API_URL } from "../utils/constants"
 import ArticleCard from "../components/ArticleCard"
+console.log("API_URL:", API_URL)
 
 function FeedPage() {
   const { user, isLoggedIn, isLoading } = useContext(AuthContext)
 
   const [articles, setArticles] = useState([])
+  console.log("articles:", articles)
   const { getToken } = useContext(AuthContext)
 
   const getAllArticles = () => {
     axios
-      .get(`${API_URL}/api/articles`, {})
+      .get(`${API_URL}/articles`)
       .then((response) => setArticles(response.data))
       .catch((error) => console.log(error))
   }
@@ -28,6 +30,7 @@ function FeedPage() {
       {/* <p>You are logged in as {user.payload.username} </p> */}
 
       {articles.map((article) => {
+        console.log("article:", article)
         return <ArticleCard key={article._id} {...article} />
       })}
     </div>
