c1a1a933893379cdda621962460a4b07f60235d7
Hugo Cornu <cornu.hugo.p@gmail.com>
2022-07-06 12:51:12 +0200

    Filter by contries DONE

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,36 +1,17 @@
-import { useContext, useEffect, useState } from "react"
-import axios from "axios"
-import { AuthContext } from "../context/auth.context"
-import { Link, Navigate, useSearchParams } from "react-router-dom"
+import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
 import SearchCountryBar from "../components/SearchCountryBar"
 
 function SearchPage() {
-  const { getToken } = useContext(AuthContext)
-  const storedToken = getToken()
   // Declare state for the search bar component
   const [searchParams, setSearchParams] = useSearchParams()
   const [countriesList, setCountriesList] = useState([])
 
-  const selectedCountries = searchParams.get("countries")?.split(",")
+  const selectedCountries = searchParams.get("countries")?.split(",") || []
   const setSelectedCountries = (values) => {
     setSearchParams({ countries: values.join(",") })
   }
 
-  // // Get articles and set them
-  // useEffect(() => {
-  //   axios
-  //     .get(`${API_URL}/articles/`, {
-  //       params: searchParams,
-  //       headers: { Authorization: `Bearer ${storedToken}` },
-  //     })
-  //     .then((response) => {
-  //       setArticlesToDisplay(response.data)
-  //     })
-  //     .catch((e) => {
-  //       console.log(e)
-  //     })
-  // }, [searchParams, storedToken])
-  // SearchParamas
   return (
     <>
       <div>SEARCH BAR</div>
