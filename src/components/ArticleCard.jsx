0357202f88f390c7c33b86e9d62548d7a7acce67
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-07-06 17:47:50 +0200

    add styling to post in feed + country tag

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import ArticleInteraction from "./ArticleInteraction"
+import { CheckCircleIcon, LocationMarkerIcon } from "@heroicons/react/solid"
 
 function ArticleCard({
   title,
@@ -12,35 +13,54 @@ function ArticleCard({
   getAllArticles,
   setFavorites,
 }) {
-  console.log("------ ARTICLE TITLE ---------:", title)
   return (
-    <article className="ArticleCard">
-      <div className="author-info">
-        <div className="author-img">
-          <img src={author?.image} alt="" />
-        </div>
+    <article className=" max-w-md rounded-lg shadow border mb-8">
+      <div className=" ml-2 mt-2 mb-4">
+        <div className="author-img"></div>
         <div className="author-name">
-          <Link to={`/${author?.username}`}>{author?.username}</Link>
+          <Link to={`/${author?.username}`}>
+            <div className="flex text-left">
+              <div className=" mt-2 rounded-full h-10 w-10 bg-gray-00 border-violet-600 border-2 flex items-center justify-center overflow-hidden">
+                <img src={author?.image} alt="" />
+              </div>
+              <div>
+                <div className="pt-1 ml-2 font-semibold text-md block">
+                  {" "}
+                  {author?.name}{" "}
+                  <span>
+                    <CheckCircleIcon className="h-4 text-violet-600 inline ml-1" />
+                  </span>
+                </div>
+                <div className=" ml-2 text-sm block"> @{author?.username}</div>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
       <picture className="article-img">
-        <img src={image} alt="" />
+        <img src={image} className=" h-96 w-96 object-cover" alt="" />
       </picture>
-      <div className="article-interactions">
-        <ArticleInteraction
-          _id={_id}
-          isFav={isFav}
-          getAllArticles={getAllArticles}
-          setFavorites={setFavorites}
-        />
-        <div className="article-location">
-          {city?.cityName}, {city?.country?.countryName}
-        </div>
-      </div>
 
-      <div className="article-text">
-        <h3>{title}</h3>
-        <div className="article-description">{description}</div>
+      <div className="mx-4 mt-2">
+        <div className="flex justify-between">
+          <ArticleInteraction
+            _id={_id}
+            isFav={isFav}
+            getAllArticles={getAllArticles}
+            setFavorites={setFavorites}
+          />
+          <Link to={`?countries=${city.country.countryCca2}`}>
+            <div className=" inline-block bg-purple-100 text-purple-800 text-xs font-semibold mr-2 px-2.5 py-1 mt-1 rounded">
+              <LocationMarkerIcon className="mr-1 -mt-1 w-4 h-4 inline-block" />
+              {city.country.countryName}
+            </div>
+          </Link>
+        </div>
+
+        <div className="text-left mt-2 mb-4">
+          <h3 className="text-md font-bold mb-2">{title}</h3>
+          <div className="article-description">{description}</div>
+        </div>
       </div>
     </article>
   )
--- src/pages/FeedPage.js
+++ src/pages/FeedPage.js
@@ -71,6 +71,7 @@ function FeedPage() {
       ) : (
         <section className="FeedPage relative mt-24 w-max m-auto">
           {articleWithFavorites.map((article) => {
+            console.log("article:", article)
             return (
               <ArticleCard
                 key={article._id}
