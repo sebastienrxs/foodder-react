e4f7aabf16f010f34389deeaa400be5bc5e45708
Sébastien R <103518570+sebastienrxs@users.noreply.github.com>
2022-07-14 18:29:23 +0200

    edit HP + navbar styling

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,10 +1,15 @@
+import { LogoutIcon } from "@heroicons/react/outline"
 import profilePlaceholder from "../assets/profile-placeholder.jpg"
 
 const ProfileHeader = ({ image, username, name, numberOfArticles }) => {
   return (
     <>
       <div className="max-w-lg w-96 mb-6 bg-white rounded-lg border border-gray-200 shadow-md ">
-        <div className="flex justify-end px-4 pt-4"></div>
+        <div className="flex justify-end px-4 pt-4">
+          {/* <button className="h-16 px-6 flex justify-end items-center w-full">
+            <LogoutIcon className=" text-gray-800 w-6 h-6" />
+          </button> */}
+        </div>
         <div className="flex flex-col items-center pb-10">
           <img
             className="mb-3 w-24 h-24 border-solid border-blue-600 border-2 rounded-full shadow-lg"
